3d01225344db23d32b051ec1385d63a270fc1d03
jbyrnes <jon.byrnes>
2014-12-23 21:33:34 -0500

    object logger example

---
+++ src/main/java/biz/directnorth/util/ObjectLogger.java
@@ -0,0 +1,23 @@
+package biz.directnorth.util;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ObjectLogger {
+
+	private static final Log log = LogFactory.getLog(ObjectMapper.class);	
+	private static final ObjectMapper mapper = new ObjectMapper();
+	
+    public Object toJson(Object payload) {   
+    	try {
+			log.info(mapper.writeValueAsString(payload));			
+		} catch (JsonProcessingException e) {
+			log.error(e.getMessage(), e);
+		}
+        return payload;
+    }
+  
+}
